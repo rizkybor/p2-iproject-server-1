1d5d94eb8288560d071089a6bafa7f45e664609a
rizkybor <rizkyak994@gmail.com>
2021-12-16 12:15:35 +0700

    config deploy

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== "production") { require("dotenv").config(); } 
 const express = require('express')
 const routes = require('./routes')
 const app = express()
-const port = process.env.PORT || 3000
+
 const cors = require('cors')
 
 app.use(cors())
